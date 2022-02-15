a95994bf3770318e11ed65fcdfccb7875d727499
JaeHyun Lee <myleje@gmail.com>
2022-02-15 16:25:24 +0900

    .

--- Assets/02.Scripts/MummyRay.cs
+++ Assets/02.Scripts/MummyRay.cs
@@ -5,17 +5,25 @@ using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
 
-public class MummyRay : MonoBehaviour
+public class MummyRay : Agent
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Initialize()
     {
-
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnEpisodeBegin()
     {
+    }
 
+    public override void CollectObservations(VectorSensor sensor)
+    {
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
     }
 }
