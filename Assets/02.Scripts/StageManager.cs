3b91dc435ea4aa4882d84add852cd8e5e2414012
JaeHyun Lee <myleje@gmail.com>
2022-02-15 16:44:39 +0900

    .

--- Assets/02.Scripts/StageManager.cs
+++ Assets/02.Scripts/StageManager.cs
@@ -20,19 +20,35 @@ public class StageManager : MonoBehaviour
         // GoodItem 생성
         for (int i = 0; i < goodItemCount; i++)
         {
+            // 불규칙한 위치 생성
             Vector3 pos = new Vector3(Random.Range(-22.0f, 22.0f)
                                     , 0.0f
                                     , Random.Range(-22.0f, 22.0f));
+            // 불규칙한 회전
+            Quaternion rot = Quaternion.Euler(Vector3.up * Random.Range(0, 360));
+
+            Instantiate(goodItem, pos, rot, transform);
         }
 
         // BadItem 생성
+        for (int i = 0; i < badItemCount; i++)
+        {
+            // 불규칙한 위치 생성
+            Vector3 pos = new Vector3(Random.Range(-22.0f, 22.0f)
+                                    , 0.0f
+                                    , Random.Range(-22.0f, 22.0f));
+            // 불규칙한 회전
+            Quaternion rot = Quaternion.Euler(Vector3.up * Random.Range(0, 360));
+
+            Instantiate(badItem, pos, rot, transform);
+        }
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        InitStage();
     }
 
 }
